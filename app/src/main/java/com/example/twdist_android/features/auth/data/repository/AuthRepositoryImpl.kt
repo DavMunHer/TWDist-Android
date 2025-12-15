d12d8ddcd49fa62d8dd8068610ffa3e4dfb41182
DavMunHer <davidmunyozh@gmail.com>
2025-12-15 23:03:28 +0100

    feat: added last files for being able to include all logic of a feature

--- app/src/main/java/com/example/twdist_android/features/auth/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/twdist_android/features/auth/data/repository/AuthRepositoryImpl.kt
@@ -3,3 +3,5 @@ package com.example.twdist_android.features.auth.data.repository
 // This file will manage all the data (domain) layer, therefore making the http requests and this kinda stuff
 // This layer is not responsible for changing the logic of the UI, only the persistent data (db located) and requests
 // This implements the AuthRepository interface and applies the logic needed for each method
+
+// This layer will use the DTOs and the UserApi (property of the class that will be used for making the requests)
---
+++ app/src/main/java/com/example/twdist_android/features/auth/presentation/screens/LoginScreenWithViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.twdist_android.features.auth.presentation.screens
+
+/*
+
+@Composable
+fun LoginScreenWithViewModel(viewModel: LoginViewModel) {
+    LoginScreen(
+        formState = viewModel.formState,
+        onUsernameChange = viewModel::onUsernameChange,
+        same for email, pwd, etc.,
+        onCreateClick = viewModel::onCreateClick
+    )
+}
+ */
