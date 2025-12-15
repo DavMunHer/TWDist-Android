a9c334a17b02aa769bea5e505ee9cc0fda84d3d8
DavMunHer <davidmunyozh@gmail.com>
2025-12-15 23:07:56 +0100

    fix: added return type to AuthRepository

--- app/src/main/java/com/example/twdist_android/features/auth/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/example/twdist_android/features/auth/domain/repository/AuthRepository.kt
@@ -1,7 +1,9 @@
 package com.example.twdist_android.features.auth.domain.repository
 
+import com.example.twdist_android.features.auth.domain.model.User
+
 interface AuthRepository {
     // This is for defining the actions that this feature can do regarding the data layer (requests to back and stuff)
 
-    fun createUser();
+    fun createUser(): User; // For returning the User model type, we will use the toDomain() func from the mapper (UserResponse)
 }
