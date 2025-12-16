182dd6ef8d51070c7dea2851848195930a725cc4
DavMunHer <davidmunyozh@gmail.com>
2025-12-16 21:54:32 +0100

    feat: created bottom bar item data type

---
+++ app/src/main/java/com/example/twdist_android/core/ui/navigation/BottomBarItem.kt
@@ -0,0 +1,10 @@
+package com.example.twdist_android.core.ui.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+data class BottomBarItem(
+    val route: String,
+    val label: String,
+    val icon: ImageVector
+)
