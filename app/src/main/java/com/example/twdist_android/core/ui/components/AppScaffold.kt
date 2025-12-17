367db867805fed6877ca1d26b7eb5e004a3d43c8
DavMunHer <davidmunyozh@gmail.com>
2025-12-17 11:00:04 +0100

    feat: added preview for AppScaffold

--- app/src/main/java/com/example/twdist_android/core/ui/components/AppScaffold.kt
+++ app/src/main/java/com/example/twdist_android/core/ui/components/AppScaffold.kt
@@ -7,8 +7,10 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.twdist_android.core.ui.navigation.BottomBar
 import com.example.twdist_android.core.ui.navigation.BottomBarItem
 
@@ -31,4 +33,10 @@ fun AppScaffold(content: @Composable () -> Unit) {
             content()
         }
     }
+}
+
+@Preview
+@Composable
+fun AppScaffoldPreview() {
+    AppScaffold { Text("Preview text (Here is where all the UI should be)") }
 }
