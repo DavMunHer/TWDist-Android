277fc643d9a5c5f735850353ec109b2f47997bdb
DavMunHer <davidmunyozh@gmail.com>
2025-12-17 10:58:30 +0100

    feat: created AppScaffold containing the BottomBar

---
+++ app/src/main/java/com/example/twdist_android/core/ui/components/AppScaffold.kt
@@ -0,0 +1,34 @@
+package com.example.twdist_android.core.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.twdist_android.core.ui.navigation.BottomBar
+import com.example.twdist_android.core.ui.navigation.BottomBarItem
+
+@Composable
+fun AppScaffold(content: @Composable () -> Unit) {
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                items = listOf(
+                    BottomBarItem("today", "Today", Icons.Default.DateRange),
+                    BottomBarItem("upcoming", "Upcoming", Icons.Default.DateRange),
+                    BottomBarItem("favorites", "Favorites", Icons.Default.Star),
+                    BottomBarItem("more", "More options", Icons.Default.Menu)
+                ),
+                onNavigationClick = {}
+            )
+        }
+    ) { innerPadding ->
+        Row(Modifier.padding(innerPadding)) {
+            content()
+        }
+    }
+}
