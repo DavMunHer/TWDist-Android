83e0eced9c23bc71001c06e90a5c83661ff76c52
DavMunHer <davidmunyozh@gmail.com>
2026-01-24 18:59:52 +0100

    feat: implemented onSubmit method in the viewModel

--- app/src/main/java/com/example/twdist_android/features/auth/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/twdist_android/features/auth/presentation/viewmodel/LoginViewModel.kt
@@ -1,11 +1,17 @@
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.twdist_android.features.auth.data.dto.LoginRequestDto
+import com.example.twdist_android.features.auth.domain.usecases.LoginUseCase
 import com.example.twdist_android.features.auth.presentation.model.LoginFormState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel(
+    private val loginUseCase: LoginUseCase
+): ViewModel() {
     private val _uiState = MutableStateFlow(LoginFormState())
 
     val uiState: StateFlow<LoginFormState> = _uiState.asStateFlow()
@@ -19,8 +25,19 @@ class LoginViewModel: ViewModel() {
     }
 
     fun onSubmit() {
-        val state = _uiState.value;
+        val state = _uiState.value
 
-        // TODO: Send form state with repo logic
+        viewModelScope.launch {
+            try {
+                loginUseCase(LoginRequestDto(
+                    state.email,
+                    state.password
+                ))
+            } catch (e: Exception) {
+                println("An error occurred")
+                // FIXME: Add error handling to give feedback to user
+                //  (error message in state)
+            }
+        }
     }
 }
