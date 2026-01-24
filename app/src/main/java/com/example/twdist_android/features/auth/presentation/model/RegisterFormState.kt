2f98794c0d25d39acad245ff56d5a114a3c3a88e
Oscar Barber Canet <oscarbarber2004@gmail.com>
2026-01-24 14:56:49 +0100

    feat: Created viewModel for register

--- app/src/main/java/com/example/twdist_android/features/auth/presentation/model/RegisterFormState.kt
+++ app/src/main/java/com/example/twdist_android/features/auth/presentation/model/RegisterFormState.kt
@@ -1,7 +1,7 @@
 package com.example.twdist_android.features.auth.presentation.model
 
 data class RegisterFormState(
-    val email: String,
-    val username: String,
-    val password: String
+    val email: String = "",
+    val username: String = "",
+    val password: String = ""
 )
--- app/src/main/java/com/example/twdist_android/features/auth/presentation/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/twdist_android/features/auth/presentation/viewmodel/RegisterViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.twdist_android.features.auth.presentation.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.twdist_android.features.auth.presentation.model.RegisterFormState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RegisterViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(RegisterFormState())
+
+    val uiState: StateFlow<RegisterFormState> = _uiState.asStateFlow()
+
+    fun updateEmail(newEmail: String) {
+        _uiState.update { it.copy(email = newEmail) }
+    }
+
+    fun updateUsername(newUsername: String) {
+        _uiState.update { it.copy(email = newUsername) }
+    }
+
+    fun updatePassword(newPassword: String) {
+        _uiState.update { it.copy(email = newPassword) }
+    }
+}
