3dd644a1a36246bb82400b6b107b424147917ca5
Oscar Barber Canet <oscarbarber2004@gmail.com>
2026-01-29 13:59:08 +0100

    fix: update Project data class to use Long for id and add role and isFavorite properties

--- app/src/main/java/com/example/twdist_android/features/explore/domain/model/Project.kt
+++ app/src/main/java/com/example/twdist_android/features/explore/domain/model/Project.kt
@@ -1,8 +1,8 @@
 package com.example.twdist_android.features.explore.domain.model
 
 data class Project(
-    val id: String,
+    val id: Long,
     val name: String,
-    val description: String,
-    val imageUrl: String
+    val role: String? = null,
+    val isFavorite: Boolean = false
 )
