34a1c358da6ea5d73857901832cad63dbeefc848
Oscar Barber Canet <oscarbarber2004@gmail.com>
2026-01-29 13:42:58 +0100

    feat: add Project data class

---
+++ app/src/main/java/com/example/twdist_android/features/explore/domain/model/Project.kt
@@ -0,0 +1,8 @@
+package com.example.twdist_android.features.explore.domain.model
+
+data class Project(
+    val id: String,
+    val name: String,
+    val description: String,
+    val imageUrl: String
+)
