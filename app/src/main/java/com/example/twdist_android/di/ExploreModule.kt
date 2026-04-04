d2947199618d0c974f2fbe34636c8a480a3fe919
DavMunHer <davidmunyozh@gmail.com>
2026-04-04 13:35:18 +0200

    fix: updated explore module for DI with newer repositories and stores

--- app/src/main/java/com/example/twdist_android/di/ExploreModule.kt
+++ app/src/main/java/com/example/twdist_android/di/ExploreModule.kt
@@ -3,8 +3,12 @@ package com.example.twdist_android.di
 import com.example.twdist_android.features.explore.data.remote.ExploreApi
 import com.example.twdist_android.features.explore.data.repository.ProjectRepositoryImpl
 import com.example.twdist_android.features.explore.data.repository.SectionRepositoryImpl
+import com.example.twdist_android.features.explore.data.store.inmemory.InMemoryProjectStore
+import com.example.twdist_android.features.explore.data.store.inmemory.InMemorySectionStore
 import com.example.twdist_android.features.explore.domain.repository.ProjectRepository
 import com.example.twdist_android.features.explore.domain.repository.SectionRepository
+import com.example.twdist_android.features.explore.domain.store.ProjectStateStore
+import com.example.twdist_android.features.explore.domain.store.SectionStateStore
 import com.example.twdist_android.features.explore.domain.usecases.CreateProjectUseCase
 import com.example.twdist_android.features.explore.domain.usecases.GetProjectsUseCase
 import dagger.Module
@@ -19,21 +23,39 @@ object ExploreModule {
 
     @Provides
     @Singleton
-    fun provideProjectRepository(api: ExploreApi): ProjectRepository =
+    fun provideProjectStateStore(): ProjectStateStore = InMemoryProjectStore()
+
+    @Provides
+    @Singleton
+    fun provideSectionStateStore(): SectionStateStore = InMemorySectionStore()
+
+    @Provides
+    @Singleton
+    fun provideProjectRepository(
+        api: ExploreApi
+    ): ProjectRepository =
         ProjectRepositoryImpl(api)
 
     @Provides
     @Singleton
-    fun provideSectionRepository(api: ExploreApi): SectionRepository =
+    fun provideSectionRepository(
+        api: ExploreApi
+    ): SectionRepository =
         SectionRepositoryImpl(api)
 
     @Provides
     @Singleton
-    fun provideGetProjectsUseCase(repository: ProjectRepository): GetProjectsUseCase =
-        GetProjectsUseCase(repository)
+    fun provideGetProjectsUseCase(
+        repository: ProjectRepository,
+        projectStateStore: ProjectStateStore
+    ): GetProjectsUseCase =
+        GetProjectsUseCase(repository, projectStateStore)
 
     @Provides
     @Singleton
-    fun provideCreateProjectUseCase(repository: ProjectRepository): CreateProjectUseCase =
-        CreateProjectUseCase(repository)
+    fun provideCreateProjectUseCase(
+        repository: ProjectRepository,
+        projectStateStore: ProjectStateStore
+    ): CreateProjectUseCase =
+        CreateProjectUseCase(repository, projectStateStore)
 }
