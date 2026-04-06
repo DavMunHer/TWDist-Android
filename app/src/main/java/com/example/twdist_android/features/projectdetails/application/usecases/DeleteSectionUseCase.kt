099164cf38c14fa8083da5a00e95f2b44c62d0a0
Oscar Barber Canet <oscarbarber2004@gmail.com>
2026-04-06 17:45:42 +0200

    feat: add use cases for section rename and delete

---
+++ app/src/main/java/com/example/twdist_android/features/projectdetails/application/usecases/DeleteSectionUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.twdist_android.features.projectdetails.application.usecases
+
+import com.example.twdist_android.features.projectdetails.domain.repository.SectionRepository
+import javax.inject.Inject
+
+class DeleteSectionUseCase @Inject constructor(
+    private val repository: SectionRepository
+) {
+    suspend operator fun invoke(sectionId: Long): Result<Unit> {
+        return repository.deleteSection(sectionId)
+    }
+}
